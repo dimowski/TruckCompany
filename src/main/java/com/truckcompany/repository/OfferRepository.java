44bf000bcdb1f8b41bc8a51bef93d8d86999437d
Dmitry Kachurowski <dimaka4@gmail.com>
2016-12-14 13:00:51 +0300

    Update application-prod.yml

--- src/main/java/com/truckcompany/repository/OfferRepository.java
+++ src/main/java/com/truckcompany/repository/OfferRepository.java
@@ -2,14 +2,9 @@ package com.truckcompany.repository;
 
 import com.truckcompany.domain.Company;
 import com.truckcompany.domain.Offer;
-import org.springframework.boot.devtools.remote.server.Dispatcher;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.util.List;
 
 /**
  * Created by Viktor Dobroselsky on 01.11.2016.
